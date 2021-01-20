acdd9ebeb31b1a45cc5494a477ba7e63b09da914
Edison Garcia <egachi@live.com>
2021-01-20 07:53:08 -0600

    Initial Commit

--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
 #!bin/bash
-
-export GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' --chdir=/home/site/wwwroot" gunicorn -b 0.0.0.0 -k sync app:app
+gunicorn -b 0.0.0.0 -k sync --timeout 600 --access-logfile '-' --error-logfile '-' app:app
